10e5872203c3d68f21c236abb594a9f10c5eba00
Jonathan MirCha <jonmircha@gmail.com>
2020-01-31 14:23:38 -0600

    Agregando build App Akáshicos

---
+++ akashicos/precache-manifest.d20b06784bdd907875901858ea78068c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40a520b6b5ce442ed8f649ec92694f4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0c7b33310258a4219bf",
+    "url": "./static/css/main.2792bdff.chunk.css"
+  },
+  {
+    "revision": "1ecf011819e2495feec7",
+    "url": "./static/js/2.8c8e0296.chunk.js"
+  },
+  {
+    "revision": "a59a40e6da71e996f0700f95047e20da",
+    "url": "./static/js/2.8c8e0296.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0c7b33310258a4219bf",
+    "url": "./static/js/main.8cd5862d.chunk.js"
+  },
+  {
+    "revision": "0831e8dadc3fb3954e71",
+    "url": "./static/js/runtime-main.50a15f32.js"
+  }
+]);
