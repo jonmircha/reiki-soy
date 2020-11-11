5ec92c46c2b8e089a69633d2e18f80ecd97c0320
Jonathan MirCha <jonmircha@gmail.com>
2020-11-11 00:05:12 -0600

    Akashicos 2

--- akashicos/precache-manifest.70eee55ed04bf51d4a3b33bd7937697c.js
+++ akashicos/precache-manifest.70eee55ed04bf51d4a3b33bd7937697c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "27cb2806a83d4a0381d929f54130a1f0",
+    "revision": "273a578b14e0695741aa825d627b343e",
     "url": "./index.html"
   },
   {
-    "revision": "0066560b3c72ac988564",
+    "revision": "5d0a8c628126530afc0a",
     "url": "./static/css/main.762bae44.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8c8e0296.chunk.js.LICENSE"
   },
   {
-    "revision": "0066560b3c72ac988564",
-    "url": "./static/js/main.8bda18ce.chunk.js"
+    "revision": "5d0a8c628126530afc0a",
+    "url": "./static/js/main.b0d353aa.chunk.js"
   },
   {
     "revision": "0831e8dadc3fb3954e71",
--- akashicos/service-worker.js
+++ akashicos/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ccb681684aa0783892c6db3d57dd81f4.js"
+  "./precache-manifest.70eee55ed04bf51d4a3b33bd7937697c.js"
 );
 
 self.addEventListener('message', (event) => {
